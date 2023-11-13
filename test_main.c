c921af6f8be9c872a07d6dbc701ff4a0bfcbac5b
amine-zzr <aminezoukri@gmail.com>
2023-11-13 18:25:10 +0000

    a simple modification to test the lenght returned by the function

--- test_main.c
+++ test_main.c
@@ -3,10 +3,12 @@
 
 int main(void)
 {
+	int len1 , len2;
 	char c = 'a';
 
-	_printf("this is [%c] test to our [%s] function\n", c, "costum printf");
-	printf("this is [%c] test to our [%s] function\n", c, "costum printf");
-
+	len1 = _printf("this is [%c] test to our [%s] function\n", c, "costum printf");
+	len2 = printf("this is [%c] test to our [%s] function\n", c, "costum printf");
+	
+	printf("_printf: [%d]\n printf: [%d]\n", len1, len2);
 	return (0);
 }
