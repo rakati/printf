f69b063f0818d1aa9601ec66812192d2a98a5541
amine-zzr <aminezoukri@gmail.com>
2023-11-15 00:01:59 +0000

    adding prototypes for hexadecimal functions

--- main.h
+++ main.h
@@ -10,6 +10,8 @@ int check_type(char *ptr, va_list arg);
 int print_str(char *s);
 int _putchar(int c);
 int print_num(long n, int base);
+int print_hex(long x, int base);
+int print_HEX(long x, int base);
 int _printf(const char *format, ...);
 
 #endif
